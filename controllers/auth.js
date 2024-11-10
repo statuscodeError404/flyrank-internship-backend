654e86e5729cdc442d4c1af1bcb4aee6bba82fdb
ststuscodeError404 <mirzaomanovic355@gmail.com>
2024-11-10 19:06:06 +0100

    added user login and get json web token

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/User');
 
 
 // @desc   Register user
-// @route  GET /api/v1/auth/register
+// @route  POST /api/v1/auth/register
 // @access Public
 
 exports.register = asyncHandler(async (req, res, next) => {
@@ -18,5 +18,41 @@ exports.register = asyncHandler(async (req, res, next) => {
         role
     });
 
-    res.status(200).json({ success: true});
+    // Create token
+
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({ success: true, token});
+});
+
+// @desc   Login user
+// @route  POST /api/v1/auth/Login
+// @access Public
+
+exports.login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // Validate email and password
+    if (!email || !password) {
+        return next(new ErrorResponse('Please provide an email and password', 400));
+    }
+
+    // Check for user in the database
+    const user = await User.findOne({ email }).select('+password');
+
+    // Check if user exists
+    if (!user) {
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
+
+    // Match password
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
+
+    // Create token
+    const token = user.getSignedJwtToken();
+    res.status(200).json({ success: true, token });
 });
